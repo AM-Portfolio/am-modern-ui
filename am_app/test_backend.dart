7cd76beaefc61924531e045b6bae9ce57706c335
asraxsoumya <soumyadip.dasadhikari@asrax.in>
2026-07-23 14:31:11 +0530

    fix(pr-review): remove debug print statement in test_backend.dart

--- am_app/test_backend.dart
+++ am_app/test_backend.dart
@@ -2,7 +2,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() async {
-  print('Testing connection to http://localhost:9000/identity/auth/login');
   try {
     final response = await http.post(
       Uri.parse('http://localhost:9000/identity/auth/login'),
